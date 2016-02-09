728ae5afd9521383c639a9d5f460199c0c04ed8c
teja-rostan <teja.rostan@gmail.com>
2016-02-09 16:56:42 +0100

    Popravljeno rezanje nevronske mreze in dodan delez rezanja.

--- nnet_fusion_deep.py
+++ nnet_fusion_deep.py
@@ -48,40 +48,53 @@ def dropout(X, p=0.):
     return X
 
 
-def fusion_update(w_h, w_h2, w_o, rank):
+def fusion_update(w_h, w_h2, w_h3, w_h4, w_o, rank):
     t1 = fusion.ObjectType('Type 1', rank)
     t2 = fusion.ObjectType('Type 2', rank)
     t3 = fusion.ObjectType('Type 3', rank)
     t4 = fusion.ObjectType('Type 4', rank)
+    t5 = fusion.ObjectType('Type 5', rank)
+    t6 = fusion.ObjectType('Type 6', rank)
     relations = [fusion.Relation(w_h.get_value(), t1, t2),
                  fusion.Relation(w_h2.get_value(), t2, t3),
-                 fusion.Relation(w_o.get_value(), t3, t4)]
+                 fusion.Relation(w_h3.get_value(), t3, t4),
+                 fusion.Relation(w_h4.get_value(), t4, t5),
+                 fusion.Relation(w_o.get_value(), t5, t6)]
     fusion_graph = fusion.FusionGraph()
     fusion_graph.add_relations_from(relations)
     fuser = fusion.Dfmf()
     fuser.fuse(fusion_graph)
-
-    threshold = np.mean([np.percentile(abs(fuser.complete(relations[0])) - abs(w_h.get_value()), 20),
-                         np.percentile(abs(fuser.complete(relations[1])) - abs(w_h2.get_value()), 20),
-                         np.percentile(abs(fuser.complete(relations[2])) - abs(w_o.get_value()), 20)])
+    threshold = np.mean([np.percentile(abs(fuser.complete(relations[0])) - abs(w_h.get_value()), 10),
+                         np.percentile(abs(fuser.complete(relations[1])) - abs(w_h2.get_value()), 10),
+                         np.percentile(abs(fuser.complete(relations[2])) - abs(w_h3.get_value()), 10),
+                         np.percentile(abs(fuser.complete(relations[3])) - abs(w_h4.get_value()), 10),
+                         np.percentile(abs(fuser.complete(relations[4])) - abs(w_o.get_value()), 10)])
     c1 = (abs(fuser.complete(relations[0])) - (abs(w_h.get_value()))) < threshold
     c2 = (abs(fuser.complete(relations[1])) - (abs(w_h2.get_value()))) < threshold
-    c3 = (abs(fuser.complete(relations[2])) - (abs(w_o.get_value()))) < threshold
-    deg=1-(np.count_nonzero(c1)+np.count_nonzero(c2)+np.count_nonzero(c3))/float(len(c1.flat)+len(c2.flat)+len(c3.flat))
+    c3 = (abs(fuser.complete(relations[2])) - (abs(w_h3.get_value()))) < threshold
+    c4 = (abs(fuser.complete(relations[3])) - (abs(w_h3.get_value()))) < threshold
+    c5 = (abs(fuser.complete(relations[4])) - (abs(w_o.get_value()))) < threshold
+    deg = 1 - (np.count_nonzero(c1) + np.count_nonzero(c2) + np.count_nonzero(c3) + np.count_nonzero(
+            c4) + np.count_nonzero(c5)) / float(
+        len(c1.flat) + len(c2.flat) + len(c3.flat) + len(c4.flat) + len(c5.flat))
 
+    w_h.set_value(w_h.get_value() * c1)
+    w_h2.set_value(w_h2.get_value() * c2)
+    w_h3.set_value(w_h3.get_value() * c3)
+    w_h4.set_value(w_h4.get_value() * c4)
+    w_o.set_value(w_o.get_value() * c5)
+    return deg, threshold, c1, c2, c3, c4, c5
+
+
+def repair(w_h, w_h2, w_h3, w_h4, w_o, c1, c2, c3, c4, c5):
     w_h.set_value(w_h.get_value()*c1)
     w_h2.set_value(w_h2.get_value()*c2)
-    w_o.set_value(w_o.get_value()*c3)
-    return deg, threshold, c1, c2, c3
+    w_h3.set_value(w_h3.get_value()*c3)
+    w_h4.set_value(w_h4.get_value()*c4)
+    w_o.set_value(w_o.get_value()*c5)
 
 
-def repair(w_h, w_h2, w_o, c1, c2, c3):
-    w_h.set_value(w_h.get_value()*c1)
-    w_h2.set_value(w_h2.get_value()*c2)
-    w_o.set_value(w_o.get_value()*c3)
-
-
-def model(X, w_h, w_h2, w_o, p_drop_input, p_drop_hidden):
+def model(X, w_h, w_h2, w_h3, w_h4, w_o, p_drop_input, p_drop_hidden):
     X = dropout(X, p_drop_input)
     h = rectify(T.dot(X, w_h))
 
@@ -89,11 +102,19 @@ def model(X, w_h, w_h2, w_o, p_drop_input, p_drop_hidden):
     h2 = rectify(T.dot(h, w_h2))
 
     h2 = dropout(h2, p_drop_hidden)
-    py_x = softmax(T.dot(h2, w_o))
-    return h, h2, py_x
+    h3 = rectify(T.dot(h2, w_h3))
+
+    h3 = dropout(h3, p_drop_hidden)
+    h4 = rectify(T.dot(h3, w_h4))
+
+    h4 = dropout(h4, p_drop_hidden)
+    py_x = softmax(T.dot(h4, w_o))
+    return h, h2, h3, h4, py_x
+
 
 (trX, trY), (teX, teY), num_of_class = load_mnist.load_data()
 row, col = np.shape(trX)
+# col = c * x * y
 rank = num_of_class / 2
 hidden = (col + num_of_class) * 2 / 3
 print "Num of hidden neurons:", hidden
@@ -104,14 +125,16 @@ Y = T.fmatrix()
 
 w_h = init_weights((col, hidden))  # 784 <- input
 w_h2 = init_weights((hidden, hidden))
+w_h3 = init_weights((hidden, hidden))
+w_h4 = init_weights((hidden, hidden))
 w_o = init_weights((hidden, num_of_class))  # 10 <- output
 
-noise_h, noise_h2, noise_py_x = model(X, w_h, w_h2, w_o, 0.2, 0.5)
-h, h2, py_x = model(X, w_h, w_h2, w_o, 0., 0.)
+noise_h, noise_h2, noise_h3, noise_h4, noise_py_x = model(X, w_h, w_h2, w_h3, w_h4, w_o, 0.2, 0.5)
+h, h2, h3, h4, py_x = model(X, w_h, w_h2, w_h3, w_h4, w_o, 0., 0.)
 y_x = T.argmax(py_x, axis=1)
 
 cost = T.mean(T.nnet.categorical_crossentropy(noise_py_x, Y))
-params = [w_h, w_h2, w_o]
+params = [w_h, w_h2, w_h3, w_h4, w_o]
 updates = RMSprop(cost, params, lr=0.001)
 
 train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
@@ -123,15 +146,13 @@ for i in range(100):
     y_score = predict(teX)
     print "AUC:", auc.roc_auc(np.argmax(teY, axis=1), y_score)
 
-deg, thresh, c1, c2, c3 = fusion_update(w_h, w_h2, w_o, rank)
+deg, thresh, c1, c2, c3, c4, c5 = fusion_update(w_h, w_h2, w_h3, w_h4, w_o, rank)
 y_score = predict(teX)
 print "MF AUC:", auc.roc_auc(np.argmax(teY, axis=1), y_score), "deg:", np.mean(deg), "thresh:", np.mean(thresh)
 
 for i in range(50):
     for start, end in zip(range(0, len(trX), 128), range(128, len(trX), 128)):
         cost = train(trX[start:end], trY[start:end])
-        repair(w_h, w_h2, w_o, c1, c2, c3)
+        repair(w_h, w_h2, w_h3, w_h4, w_o, c1, c2, c3, c4, c5)
     y_score = predict(teX)
     print "AUC:", auc.roc_auc(np.argmax(teY, axis=1), y_score)
-
-
